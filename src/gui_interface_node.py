0e15b158d2729511b9dc7d13f5150146f896b87e
RobJMal <robjmal@gmail.com>
2023-11-24 15:27:26 +0100

    Added sliders for all of the fingers

--- src/gui_interface_node.py
+++ src/gui_interface_node.py
@@ -7,6 +7,7 @@ from std_msgs.msg import Float32MultiArray
 import tkinter as tk
 from tkinter import ttk
 
+# Numbers correspond to position in list 
 hand_finger_joint_map = {
     'index': {
         'index_mcp': 0,
@@ -49,31 +50,36 @@ class GuiInterface:
             0.0,    # Middle PIP/DIP
             0.0,    # Pinky MCP
             0.0,    # Pinky PIP/DIP
+            0.0,    # Thumb Flexion/Extension
+            0.0,    # Thumb Adduction/Abduction
+            0.0,    # Thumb MCP
+            0.0,    # Thumb PIP/DIP
         ]
 
         # Creating the GUI components
-        for i in range(3):
-            group = ttk.LabelFrame(master, text=f"Group {i+1}")
-            group.pack(padx=10, pady=10, fill="both", expand="yes")
-            self.create_slider_group(group, i)
+        for i_finger, finger_key in enumerate(hand_finger_joint_map):
+            group_frame = ttk.LabelFrame(master, text=f"{finger_key}")
+            group_frame.pack(padx=10, pady=10, fill="both", expand="yes")
+            self.create_slider_group(group_frame, i_finger, finger_key)
 
 
-    def create_slider_group(self, frame, group_index):
-        for i in range(2):
-            slider_label = ttk.Label(frame, text=f"Finger Slider {group_index*2 + i + 1}")
+    def create_slider_group(self, frame, group_index, finger):
+        finger_subcomponents_map = hand_finger_joint_map[finger]
+
+        for j_finger_subcomponent, finger_subcomponent_key in enumerate(finger_subcomponents_map):
+            slider_label = ttk.Label(frame, text=f"{finger_subcomponent_key}")
             slider_label.pack(pady=5)
-
             slider = ttk.Scale(frame, from_=0, to_=360, orient="horizontal", 
-                               command=lambda value, index=group_index*2 + i: self.update_slider_value(value, index))
+                               command=lambda value, index=finger_subcomponents_map[finger_subcomponent_key]: self.update_slider_value(value, index))
             slider.pack(pady=5)
 
     def update_slider_value(self, value, index):
-        self.slider_values[index] = float(value)
+        self.joint_angles[index] = float(value)
 
     def start_ros_publish_loop(self):
         def publish_values():
             msg = Float32MultiArray()
-            msg.data = self.slider_values
+            msg.data = self.joint_angles
             self.pub.publish(msg)
             self.master.after(50, publish_values)  # Publish at 20 Hz
 
